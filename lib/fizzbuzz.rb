e294e5ee4ade0a3c10072071a405b9817cf816f7
Muna <mmhussein125@gmail.com>
2018-09-20 15:51:04 +0100

    ammending code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(number)
-  if number == 3
-    'fizz'
-  elsif number == 5
-    'buzz'
+  if number % 3 == 0
+  'Fizz'
+  elsif number % 5 == 0
+  'Buzz'
+  else
+  number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,18 @@
 require_relative '../lib/fizzbuzz'
 describe 'fizzbuzz' do
-  it 'return "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'return "Fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
-  it 'return "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  it 'return "Buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+  it 'return "Fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'Fizz'
+  end
+  it 'return "Buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'Buzz'
+  end
+  it 'return number when passed a number which isn\'t a multiple of 3 or 5' do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
